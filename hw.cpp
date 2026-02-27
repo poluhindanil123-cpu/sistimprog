9b92704e3f7791d2c76310e3eb8df178a4ac3257
Fixim123 <poluhindanil123@gmail.com>
2026-02-27 12:23:47 +0300

    Update hw.cpp

--- hw.cpp
+++ hw.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 
-int main(int argc,char*argv[]) {  
-    //  int a;
-    //  std::cin >> a ;// std::endl;  
-    //  std::cout << "Helloworld" << std::endl;
-    //  std::cerr << "ERROR!" << std::endl;
-    //  return a;
-    
-     int nail[9];
-    for(int i = 0; i<9; i++){   
-       nail[i] = i; 
+void getHelp()
+
+int main(
+    int argc, char* argv[]){
+
+    /*std::cout<<"Hello, World!"
+            <<std::endl;
+
+    std::cerr<<"ERROR"
+            <<std::endl;
+
+    int a;
+    std::cin>>a;
+    return a;
+    */
+    /*
+    int nail[9];
+    for(int i=0; i < 9; i++){
+        nail [i] = i;
     }
-    for(int i = 0; i<9; i++){   
-       std::cout << "nail[" << i << "] = " << nail[i] << "\n"; 
-    }
-    for( int i = 0; i<argc; i++){   
-        std::cout << "argv[" << i << "] =" << argv[i] << "\n";
+
+    for(int i=0; i < 9; i++){
+        std::cout << " nail [ " << i << " ] = " << nail[i] << "\n";
+    }*/
+
+    for(int i=0; i < argc; i++){
+        std::cout << " argv [ " << i << " ] = " << argv[i] << "\n";
     }
     return 0;
 }
